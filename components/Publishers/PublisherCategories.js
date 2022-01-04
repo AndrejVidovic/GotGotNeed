942de83f7f01a8c02213d3b6daa7f0612a013c13
AndrejVidovic <vidaftw@gmail.com>
2022-01-04 21:22:48 +0100

    publishers page and specific publish page done

---
+++ components/Publishers/PublisherCategories.js
@@ -0,0 +1,44 @@
+import { Grid, Paper, Typography, useTheme } from "@mui/material";
+import dummyPublisherCollections from "../../fakeData/Publishers/PublisherCollections.json";
+import PublisherCollection from "./PublisherCollection";
+
+const styles = (theme) => ({
+    container: {
+        marginTop: "2rem",
+        // marginBottom:"2rem"
+    },
+    paper: {
+        height: "3.5rem",
+        backgroundColor: "#1976D2",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontWeight: 700,
+        fontSize: "22px",
+    },
+});
+
+function Categories({ categories }) {
+    const theme = useTheme();
+    const collections = dummyPublisherCollections;
+    return (
+        <Grid item sx={styles(theme).container}>
+            <Paper sx={styles(theme).paper}>
+                <Typography variant="body1" sx={styles(theme).title}>
+                    {categories}
+                </Typography>
+            </Paper>
+            <Paper sx={{ display: "flex" }}>
+                {collections.map((collection) => (
+                    <PublisherCollection
+                        collection={collection}
+                    ></PublisherCollection>
+                ))}
+            </Paper>
+        </Grid>
+    );
+}
+export default Categories;
---
+++ components/Publishers/PublisherCollection.js
@@ -0,0 +1,111 @@
+import {
+    Card,
+    CardContent,
+    CardMedia,
+    Grid,
+    Typography,
+    useTheme,
+} from "@mui/material";
+import Image from "next/image";
+
+const styles = (theme) => ({
+    card: {
+        display: "flex",
+        flexDirection: "row",
+        backgroundColor: "rgba(219, 219, 219, 0.08)",
+        padding: 0,
+        margin: "3rem 0.5rem 3rem 0.5rem",
+        boxShadow: theme.shadows[4],
+        width: "100rem",
+    },
+    title: {
+        fontWeight: 700,
+        fontSize: "19px",
+        marginBottom: "0.5rem",
+    },
+    info: {
+        fontWeight: 700,
+        fontSize: "12px",
+        marginBottom: "0.3rem",
+    },
+    infoValues: {
+        fontSize: "12px",
+        marginBottom: "0.3rem",
+        color: "#616161",
+        paddingLeft: "0.1rem",
+    },
+    infoGrid: {
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+    },
+    imageContainer: {
+        position: "relative",
+        width: "100%",
+        height: "100%",
+    },
+    media: {
+        width: "fill-available",
+        maxWidth: "35%",
+        minWidth: "35%",
+        padding: "0.4rem 0 0.4rem 0.4rem",
+    },
+    content: {
+        padding: "16px 0 16px 20px",
+    },
+});
+function PublisherCollection({ collection }) {
+    const theme = useTheme();
+    return (
+        <Card sx={styles(theme).card}>
+            <CardMedia sx={styles(theme).media}>
+                <Grid sx={styles(theme).imageContainer}>
+                    <Image
+                        src="/PublisherCollection.png"
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="top"
+                    />
+                </Grid>
+            </CardMedia>
+            <CardContent sx={styles(theme).content}>
+                <Typography variant="body1" sx={styles(theme).title}>
+                    {collection.title}
+                </Typography>
+                <Grid sx={styles(theme).infoGrid}>
+                    <Typography variant="body1" sx={styles(theme).info}>
+                        Release year:
+                    </Typography>
+                    <Typography sx={styles(theme).infoValues}>
+                        {collection.releaseYear}
+                    </Typography>
+                </Grid>
+                <Grid sx={styles(theme).infoGrid}>
+                    <Typography variant="body1" sx={styles(theme).info}>
+                        Number of stickers:
+                    </Typography>
+                    <Typography sx={styles(theme).infoValues}>
+                        {collection.numberOfSticker}
+                    </Typography>
+                </Grid>
+                <Grid sx={styles(theme).infoGrid}>
+                    <Typography variant="body1" sx={styles(theme).info}>
+                        Publisher:{" "}
+                    </Typography>
+                    <Typography sx={styles(theme).infoValues}>
+                        {collection.publisher}
+                    </Typography>
+                </Grid>
+                <Grid sx={styles(theme).infoGrid}>
+                    <Typography variant="body1" sx={styles(theme).info}>
+                        Category:{" "}
+                    </Typography>
+                    <Typography sx={styles(theme).infoValues}>
+                        {collection.category}
+                    </Typography>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
+}
+export default PublisherCollection;
